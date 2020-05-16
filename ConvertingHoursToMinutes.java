4bfcfe3b24f7b4a47f49b6467ff317f0fca831ae
gjover06 <georgesamu@yahoo.com>
2020-05-16 15:18:18 -0400

    Second Commit

---
+++ ConvertingHoursToMinutes.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+Author:George Samu
+// This program will convert hours to minutes
+
+public class ConvertingHoursToMinutes
+{
+	public static void main(String[] args)
+	{
+	int hour = 11;
+	int minute = 59;
+	System.out.print("Number of minutes since midnight: ");
+	System.out.println(hour * 60 + minute);
+	
+	}//end main
+	}// end ConvertingHoursToMinutes
